4d8144b4e7b044f32050afe2c50f55588a9f6812
Ivan <ivansanchez_1988@hotmail.com>
2016-11-07 00:49:20 -0500

    Grilla: Agregada grilla para separar los elementos de manera ordenada dentro de la escena

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,16 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 
@@ -21,12 +25,29 @@ public class Main extends Application {
 			
 			//Agregar elementos en un VBox para que no se superpongan
 			VBox boxVertical = new VBox();
+					
+			//Agregar un TextField
+			TextField txtFldNombre = new TextField();
+			txtFldNombre.setFont(new Font("Papyrus", 80));
+			//Agregando un Label
+			Label lblNombre = new Label("Nombre: ");
+			
+			GridPane grid = new GridPane();
+			grid.add(lblNombre, 0, 0);
+			grid.add(txtFldNombre, 1, 0);
+			grid.add(btn, 1, 1);
+			
+			//Para ver las lineas guias de la grilla
+			grid.setGridLinesVisible(true);
+			//Para espaciar los elementos
+			grid.setHgap(20);
 			
 			//Agregamos todos los elementos al VBox
-			boxVertical.getChildren().addAll(texto, btn);
+			boxVertical.getChildren().addAll(texto, grid);
 			
 			//Agregar Metodo manejador de Eventos
-			btn.setOnAction(evt -> System.out.println("Encame en proceso..."));
+			btn.setOnAction(evt -> System.out.printf("Te esta encamando... %s!%n", 
+					txtFldNombre.getText()));
 			
 			root.getChildren().add(boxVertical);
 			Scene scene = new Scene(root,400,400);
